01c32f7aaf00a7c91ebe816c675d1dde5aedbfac
James O'Neal <jamesponeal@gmail.com>
2017-02-20 11:33:12 -0800

    adds pezdispenser exercise

---
+++ javaObjects/Example.java
@@ -0,0 +1,10 @@
+public class Example {
+
+  public static void main(String[] args) {
+    // Your amazing code goes here...
+    System.out.println("We are making a new PEZ dispenser.");
+    PezDispenser dispenser = new PezDispenser("Yoda");
+    System.out.printf("The dispenser is %s %n", dispenser.getCharacterName());
+  }
+
+}
